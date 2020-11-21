3dab36b1452b685b2d5d5f7475a9bb6e139d934e
nitin-koul <nitin.koul45@gmail.com>
2020-11-21 16:42:24 +0530

    First task completed

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,11 @@ public class Calculator {
     public static int add(String str) {
         if (str.isEmpty())
             return 0;
+        else if(str.contains(","))
+        {
+            String[] numbers = str.split(",");
+            return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
+        }
         else {
             return Integer.parseInt(str);
         }
--- TestCalculator.java
+++ TestCalculator.java
@@ -1,15 +1,22 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 public class TestCalculator {
+
     @Test
     public void emptyInput()
     {
         assertEquals(0,Calculator.add(""));
     }
+
     @Test
     public void singleInput()
     {
         assertEquals(2,Calculator.add("2"));
+    }
+
+    @Test
+    public void twoNumbersInput(){
+        assertEquals(5,Calculator.add("3,2"));
     }
 
 }
